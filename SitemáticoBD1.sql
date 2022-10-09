f1c3bb74499deaa7f30227d1a2939f257bd7dc87
Anielka Areas <27larios10raquel99@gmail.com>
2022-10-08 22:33:22 -0600

    Sistematico BD1

--- SitemáticoBD1.sql
+++ SitemáticoBD1.sql
@@ -1,2 +1,63 @@
 USE AdventureWorks2019
 GO
+
+CREATE OR ALTER PROC US_Ejercisio1(
+       @NAME VARCHAR,
+	   @MODIFIEDDATE2 DATE
+)  
+AS
+BEGIN
+ DECLARE @myid uniqueidentifier = NEWID();
+ INSERT INTO Production.ProductCategory(Name,ModifiedDate)
+ VALUES
+ (
+   @NAME,@MODIFIEDDATE2
+ )
+END
+GO
+
+
+EXEC US_Ejercisio1 'Umbrella','2022/08/10'
+GO
+
+SELECT * FROM Production.ProductCategory
+GO
+
+--------------------------------------EJERCICIO 2----------------------------------------------------------------------------------
+CREATE OR ALTER  FUNCTION BirthEmployee()
+RETURNS TABLE
+AS
+RETURN
+(
+SELECT D.LoginID,D.JobTitle,D.BirthDate,D.HireDate, DATEPART(YEAR,GETDATE())- DATEPART(YEAR,D.BirthDate) AS AGE, DATEPART(YEAR,GETDATE())- DATEPART(YEAR,D.HireDate) AS ANTIGUEDAD FROM  HumanResources.Employee AS D
+);
+
+
+SELECT * FROM BirthEmployee()
+GO
+
+--------------------------------------------EJERCICIO 3 CREACION DE LOGIN---------------------------------------------
+
+CREATE LOGIN Vendedores WITH PASSWORD = 'aral'
+GO
+
+CREATE USER ARLarios FOR LOGIN Vendedores WITH DEFAULT_SCHEMA = Sales;
+GO
+
+CREATE ROLE db_Vendedores AUTHORIZATION [ARLarios]
+GO
+GRANT SELECT ON SCHEMA::Sales TO db_Vendedores
+GO
+GRANT INSERT ON SCHEMA::Sales TO db_Vendedores
+GO
+GRANT UPDATE ON SCHEMA::Sales TO db_Vendedores
+GO
+GRANT DELETE ON SCHEMA::Sales TO db_Vendedores
+GO
+GRANT SELECT ON Production.ProductCategory  TO db_Vendedores
+GO
+GRANT SELECT ON Production.Product  TO db_Vendedores
+GO
+
+ALTER ROLE db_Vendedores ADD MEMBER ARLarios
+GO
